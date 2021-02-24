f56180431bad27b1f145da2c229a6a8c58b8252d
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-02-24 11:37:32 +0100

    done with iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import contacts from "./contacts.json";
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        contacts: contacts.slice(0,5)
+    }
 }
+
+  render() {
+    const list = this.state.contacts.map(contact => {
+      console.log(contact.name)
+      return (
+        <div style={{display:'flex'}}>
+          <img style={{width: '150px', height: '150px', margin: '10px'}} src={contact.pictureUrl} alt=""/>
+          <p style={{margin: '10px'}}>{contact.name}</p>
+          <p style={{margin: '10px'}}>{contact.popularity}</p>
+        </div>
+      )  
+    })
+    console.log(list)
+    return (
+      <div className="App">
+          <h1 className="App-header">IronContacts</h1>
+          <h2>Picture Name Popularity</h2>
+          <div style={{display:'flex', flexDirection: 'column', alignItems:'flex-start', marginLeft: '50px'}}>
+            {list}
+          </div>
+      </div>
+    )  
+  }
+}
+
 
 export default App;
